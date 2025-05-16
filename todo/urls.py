48b40888625b16ff303087d0209ac91594c86f41
Sebasmv28 <munozsvillareal@gmail.com>
2025-05-16 08:02:16 -0500

    Cree el login y modifique los botones en el home

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('editar/<int:cliente_id>/', views.editar, name='editar'),
     path('eliminar/<int:cliente_id>/', views.eliminar, name='eliminar'),
     path('index/', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('registro/', views.registro_view, name='registro'),
 ]
